07843eec97e0816f48ee49bd686ff9037d241aa9
Harsh <harrsh0502@gmail.com>
2026-05-12 16:15:41 +0530

    learned about variables use in go lang

---
+++ 2_simple_values/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	// integers
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("Hello GO lang")
+
+	// boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+	fmt.Println(10.5)
+	fmt.Println(10.05 + 21.35)
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// yeh bhi use kr skte h
+	// var name string = "GoLang"
+
+	// infer
+	// var name = "GoLang"
+	var isAdult = true
+
+	fmt.Println(isAdult)
+}
